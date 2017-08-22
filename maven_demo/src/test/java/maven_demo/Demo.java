e0bc1d95f2ab4e9ef41ae3245a2897ca3127d584
john <john@idea-PC.lan>
2017-08-22 17:10:43 +0800

    第一次上传

---
+++ maven_demo/src/test/java/maven_demo/Demo.java
@@ -0,0 +1,26 @@
+/**
+ * Project Name:maven_demo
+ * File Name:Demo.java
+ * Package Name:maven_demo
+ * Date:2017年8月22日下午3:48:47
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ *
+ */
+
+package maven_demo;
+
+import org.junit.Test;
+
+/**
+ * Description: <br/>
+ * Date: 2017年8月22日 下午3:48:47 <br/>
+ * 
+ * @author dingP
+ * @version
+ * @see
+ */
+public class Demo {
+    @Test
+    public void test() {
+    }
+}
